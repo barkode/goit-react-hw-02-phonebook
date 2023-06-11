dd4d5926cf1902551715bf6d156a1fccaa874e8d
barkode <konstantinks@gmail.com>
2023-06-11 01:22:21 +0100

    add some markdown

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,40 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  handleChange = e => {
+    this.setState({ name: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.prevantDefault();
+  };
+
+  render() {
+    const { name } = this.state;
+    console.log(name);
+    return (
+      <>
+        <form>
+          Name
+          <input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            placeholder="Enter abonent name"
+            value={name}
+            onChange={this.handleChange}
+            required
+          />
+          <button type="submit">Add contact</button>
+        </form>
+      </>
+    );
+  }
+}
