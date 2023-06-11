e6f2b177e3f33bc6ebfc3fc52d1bcd70bdcc14a5
barkode <konstantinks@gmail.com>
2023-06-11 22:00:56 +0100

    Add contacts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,36 +3,45 @@ import { nanoid } from 'nanoid';
 
 export class App extends Component {
   state = {
-    contacts: [{ id: '123', name: 'joan' }],
+    contacts: [],
     name: '',
   };
 
-  handleFilterChange = () => {};
+  handleChange = e => {
+    this.setState({ name: e.target.value });
+  };
 
-  handleAddContact = newContact => {};
-
-  handleDeleteContact = () => {};
+  handleSubmit = e => {
+    e.preventDefault();
+    const { contacts, name } = this.state;
+    contacts.push({ id: nanoid(), name });
+    this.setState({ name: '' });
+  };
 
   render() {
-    const { name } = this.state;
-    console.log(name);
+    const { name, contacts } = this.state;
     return (
       <>
+        <h1>Phone book</h1>
         <form>
           Name
           <input
             type="text"
             name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             placeholder="Enter abonent name"
             value={name}
             onChange={this.handleChange}
             required
           />
-          <button type="submit" onSubmit={this.handleSubmit}>
+          <button type="submit" onClick={this.handleSubmit}>
             Add contact
           </button>
+          <h2>Contacts</h2>
+          <ul>{contacts.map(({ id, name }) => {
+            return <li key={id}>Abonent name: {name}</li>;
+          })}</ul>
         </form>
       </>
     );
