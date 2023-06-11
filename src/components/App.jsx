dbe80c6ee0f52a0207ba9c39b25f81ef8d7b9e2f
barkode <konstantinks@gmail.com>
2023-06-11 22:35:04 +0100

    add second task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,50 @@
 import { Component } from 'react';
 import { nanoid } from 'nanoid';
+import { number } from 'prop-types';
 
 export class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Jack Jones', number: '975-24-89' },
+      { id: 'id-5', name: 'Captain Morgan', number: '227-90-26' },
+      { id: 'id-6', name: 'Bilie Joe', number: '217-95-26' },
+      { id: 'id-7', name: 'Joe Foster', number: '297-99-26' },
+      { id: 'id-8', name: 'Samuel El Jackson', number: '287-04-26' },
+      { id: 'id-9', name: 'Annie Milligan', number: '154-44-28' },
+    ],
     name: '',
+    number: '',
+    filter: '',
   };
 
   handleChange = e => {
-    this.setState({ name: e.target.value });
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
   };
 
   handleSubmit = e => {
     e.preventDefault();
-    const { contacts, name } = this.state;
-    contacts.push({ id: nanoid(), name });
-    this.setState({ name: '' });
+    const { contacts, name, number } = this.state;
+    contacts.push({ id: nanoid(), name, number });
+    this.reset();
+  };
+
+  handleFilterAbonent = () => {};
+
+  reset = () => {
+    this.setState({ name: '', number: '', filter: '' });
   };
 
   render() {
-    const { name, contacts } = this.state;
+    const { name, number, contacts } = this.state;
     return (
       <>
         <h1>Phone book</h1>
         <form>
-          Name
+          Abonent name.
           <input
             type="text"
             name="name"
@@ -35,13 +55,32 @@ export class App extends Component {
             onChange={this.handleChange}
             required
           />
+          Abonent phone number
+          <input
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[\-.\s]?\(?\d{1,3}?\)?[\-.\s]?\d{1,4}[\-.\s]?\d{1,4}[\-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            placeholder="Enter phone number"
+            value={number}
+            onChange={this.handleChange}
+            required
+          />
           <button type="submit" onClick={this.handleSubmit}>
             Add contact
           </button>
           <h2>Contacts</h2>
-          <ul>{contacts.map(({ id, name }) => {
-            return <li key={id}>Abonent name: {name}</li>;
-          })}</ul>
+          Find contacts by name
+          <input></input>
+          <ul>
+            {contacts.map(({ id, name, number }) => {
+              return (
+                <li key={id}>
+                  Abonent name: {name} || Abonent number: {number}
+                </li>
+              );
+            })}
+          </ul>
         </form>
       </>
     );
